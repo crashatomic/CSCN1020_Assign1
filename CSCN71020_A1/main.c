950ed05a281a8d406e38cb297fccacd9a87ceacd
crashatomic <davidegedi2001@gmail.com>
2023-02-08 15:33:27 -0500

    proper subtract function added, along with subtract case.

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -21,8 +21,10 @@ void main() {
 	{
 	case 1:
 		add();
-	}
 
+	case 2:
+		subtract();
+	}
 }
 
 void printWelcomeMenu() {
@@ -45,4 +47,17 @@ void add() {
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract()
+{
+
+	double num1, num2, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
+
 }
